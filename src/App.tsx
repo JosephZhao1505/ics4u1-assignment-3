6242c36b479817b1c4f3c1670f290ea3a6f2f066
Joseph <joseph.zhao@student.tdsb.on.ca>
2026-03-27 02:22:50 -0400

    Began working on inputs and calculator functionality

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import { CubicEquation } from "./components/CubicEquation";
+// import { CubicGraph } from "./components/CubicGraph";
+// import { CubicHistory } from "./components/CubicHistory";
+import { CubicInput } from "./components/CubicInput";
+// import { CubicTable } from "./components/CubicTable";
+
+export const App = () => {
+  return <CubicInput />;
+  // <CubicEquation />;
+  // <CubicGraph />;
+  // <CubicTable />;
+  // <CubicHistory />;
+};
